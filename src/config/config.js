a1858531b13c43ff78c0ee9307b9ef4d1a7890e8
Matias Antilef <matias.romero.dv@gmail.com>
2025-01-01 01:56:01 -0300

    vr

--- src/config/config.js
+++
@@ -1,4 +0,0 @@
-export const SERVER_PORT = process.env.SERVER_PORT || 5000;
-export const MONGO_URI =
-  process.env.MONGO_URI ||
-  "mongodb+srv://admin:qEjSmLRyhtjZXEUV@movies.9fn9x.mongodb.net/?retryWrites=true&w=majority&appName=Movies";
